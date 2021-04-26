1df7d6dc045e6dc568838bf87adc96675fa0b51d
Hyeji Kim <hyejikim95@github.com>
2021-04-26 12:05:39 +0900

    Update handler.js

--- assets/js/handler.js
+++ assets/js/handler.js
@@ -516,36 +516,42 @@ function verifyPhoneHandler(form_p_id, checkFunc) {
 		console.log(parseInt($(form_id).find('input[name="form_phone"]').val()));
 		var jdata = {"action": "member2", "daction" : "validate_phonenumber", "phone_number" : parseInt($(form_id).find('input[name="form_phone"]').val())};
 		console.log(jdata);
-
-		$.ajax({
-			url: "https://api.duni.io/v1",
-			dataType: "json",
-			crossDomain: true,
-			cache: false,
-			data: JSON.stringify(jdata),
-			type: "POST",
-			contentType: "application/json; charset=utf-8",
-			success: function (r) {
-					if (r.result_code == "1") {      // 전송성공   			
-						showDialog("인증번호가 전송되었습니다.", null);
+		showDialog("인증번호가 전송되었습니다.", null);
 						// 인증하기 텍스트 -> 재전송
 						$(form_id + "_verify_phone").innerText = '재전송';
 						var duration = 60 * 3;
 						var display = $('#remaining_time');
 						startTimer(duration, display);	
-						//return;
-					} else if (r.code == "400") {
-						showDialog("잘못된 전화번호입니다. 다시 입력해주세요.");
-					} else if (r.code == "403") {
-						showDialog("이미 가입된 전화번호입니다. 다른번호를 입력해주세요.");
-					} else if (r.code == "500"){
-						showDialog("죄송합니다, 일시적인 오류가 발생하였습니다. 다시 시도 부탁드립니다.", null);   
-					}
-			},
-			error: function (request, status, error) { 
-				showDialog("죄송합니다, 일시적인 오류가 발생하였습니다. 다시 시도 부탁드립니다.", null);           
-				//errorcallback(request, status, error);
-			}
+
+		// $.ajax({
+		// 	url: "https://api.duni.io/v1",
+		// 	dataType: "json",
+		// 	crossDomain: true,
+		// 	cache: false,
+		// 	data: JSON.stringify(jdata),
+		// 	type: "POST",
+		// 	contentType: "application/json; charset=utf-8",
+		// 	success: function (r) {
+		// 			if (r.result_code == "1") {      // 전송성공   			
+		// 				showDialog("인증번호가 전송되었습니다.", null);
+		// 				// 인증하기 텍스트 -> 재전송
+		// 				$(form_id + "_verify_phone").innerText = '재전송';
+		// 				var duration = 60 * 3;
+		// 				var display = $('#remaining_time');
+		// 				startTimer(duration, display);	
+		// 				//return;
+		// 			} else if (r.code == "400") {
+		// 				showDialog("잘못된 전화번호입니다. 다시 입력해주세요.");
+		// 			} else if (r.code == "403") {
+		// 				showDialog("이미 가입된 전화번호입니다. 다른번호를 입력해주세요.");
+		// 			} else if (r.code == "500"){
+		// 				showDialog("죄송합니다, 일시적인 오류가 발생하였습니다. 다시 시도 부탁드립니다.", null);   
+		// 			}
+		// 	},
+		// 	error: function (request, status, error) { 
+		// 		showDialog("죄송합니다, 일시적인 오류가 발생하였습니다. 다시 시도 부탁드립니다.", null);           
+		// 		//errorcallback(request, status, error);
+		// 	}
 		});	
 		
 		$(form_id + "_verify_code").click(function(e) {
